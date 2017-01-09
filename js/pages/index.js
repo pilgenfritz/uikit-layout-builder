b21719f83feb6e5feaa3b59c4cf8e291ad89a5b8
Pedro Ilgenfritz <philgenfritz@gmail.com>
2017-01-09 09:16:34 -0200

    09/01-09:16

--- js/pages/index.js
+++ js/pages/index.js
@@ -12,23 +12,45 @@ $(document).ready(function()
 
 	//remove loader quando página carregada
 	$(window).on('load', function(){
-		setTimeout(function() {
+		/*setTimeout(function() {
 			setAlturaSlider(altura, largura);
-		}, 200);
+		}, 200);*/
 	});
 
-	var grid = UIkit.grid($('#grid'), {
+	$('.circle-line .item').on('click', function(){
+		var itemId = $(this).data('id'),
+			$parent = $(this).parent(),
+			rotatePlus = $(this).data('rotation-plus'),
+			rotateMinus = $(this).data('rotation-minus'),
+			currentRotation = $parent.rotationDegrees();
+
+			$(this).addClass('active')
+
+			if((Math.abs(currentRotation) - rotatePlus) < (Math.abs(currentRotation) - Math.abs(rotateMinus))){
+				rotationCircle = rotateMinus;
+			}else{
+				rotationCircle = rotatePlus;
+			}
+			
+			var tlCircle = new TimelineMax()
+			tlCircle
+				.to($parent, 1, {rotation: rotationCircle})
+				.to($parent.find('.item'), 0.1, {rotation: -rotationCircle}, 0)
+	})
+
+
+	/*var grid = UIkit.grid($('#grid'), {
 		gutter : 20,
 		animation: true,
 		duration: 200,
 		controls: '#filter',
 		filter: 'filter-a, filter-b'
-	});	
+	});	*/
 
 	//define a algura do header, verificando se o menu está sobre o slider
 	/*if($('#main-menu').hasClass('over_slider')) var altura = $(window).height();
 	else var altura = ($(window).height() - $('#main-menu').height());*/
-	initSlider();
+	/*initSlider();*/
 
 	//vai para a próxima seção quando clica na setinha do slider
 	/*$('.arrow-down').on('click', function(){
@@ -36,7 +58,7 @@ $(document).ready(function()
 	});*/
 
 	//Carousel
-	$('#crsl-produtos').owlCarousel({
+	/*$('#crsl-produtos').owlCarousel({
 		responsive:{
             0:{
                 items: 1
@@ -54,17 +76,43 @@ $(document).ready(function()
     		"<i class='fa fa-angle-left fa-3x nav-produtos nav-left'></i>",
     		"<i class='fa fa-angle-right fa-3x nav-produtos nav-right'></i>"
     	]
-	});
+	});*/
 
 	//do when window resizes
 	$(window).on('resize', function(){
-		newWidth = $(window).width();
+		/*newWidth = $(window).width();
 		newHeight = $(window).height();
 		
-		setAlturaSlider(newHeight, newWidth);
+		setAlturaSlider(newHeight, newWidth);*/
 	});
 
 });
+
+(function ($) {
+    $.fn.rotationDegrees = function () {
+         var matrix = this.css("-webkit-transform") ||
+    this.css("-moz-transform")    ||
+    this.css("-ms-transform")     ||
+    this.css("-o-transform")      ||
+    this.css("transform");
+    if(typeof matrix === 'string' && matrix !== 'none') {
+        var values = matrix.split('(')[1].split(')')[0].split(',');
+        var a = values[0];
+        var b = values[1];
+        var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+    } else { var angle = 0; }
+    return angle;
+   };
+}(jQuery));
+
+function getNearestNumber(a, n){
+    if((l = a.length) < 2)
+        return l - 1;
+    for(var l, p = Math.abs(a[--l] - n); l--;)
+        if(p < (p = Math.abs(a[l] - n)))
+            break;
+    return l + 1;
+}
 
 var triggerHookPos;
 if($(window).width() >= 1024){
@@ -73,22 +121,21 @@ if($(window).width() >= 1024){
 	triggerHookPos = 1;
 }
 
-		var tlSobre = new TimelineMax();
+		/*var tlSobre = new TimelineMax();
 			tlSobre.to(".section-sobre .img-sobre", 3, {autoAlpha: 1})
 				   .to(".section-sobre h1", 1, {y:0, autoAlpha: 1},0)
 				   .to(".section-sobre h3", 1, {y:0, autoAlpha: 1},0)
 				   .to(".section-sobre h2", 1, {y:0, autoAlpha: 1},0)
-				   .to(".section-sobre .rd-sobre-text", 1, {y:0, autoAlpha: 1}, "-=2")
+				   .to(".section-sobre .rd-sobre-text", 1, {y:0, autoAlpha: 1}, "-=2")*/
 
 				   
 
 		// build scene
-		var sceneSobre = new ScrollMagic.Scene({
+		/*var sceneSobre = new ScrollMagic.Scene({
 							triggerElement: ".section-sobre",
 							triggerHook: triggerHookPos,
 							duration: 0
 						})
 						.setTween(tlSobre)
-						//.addIndicators({name: "tween css class"}) // add indicators (requires plugin)
-						.addTo(controller);
+						.addTo(controller);*/
  		
