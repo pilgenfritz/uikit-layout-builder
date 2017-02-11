e5dd90311561573db189032f6948ba755070b429
Pedro Ilgenfritz <philgenfritz@gmail.com>
2017-02-11 19:38:48 -0200

    first

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-	var strFinal = '';
+	var finalHtml = '';
 
 
-	$('#radio-tipo').on('change', function(){
+	/*$('#radio-tipo').on('change', function(){
 		var tipo = $(this).find("input[type='radio']:checked").val()
 		
 		if(tipo == 'select'){
@@ -10,14 +10,80 @@ $(document).ready(function(){
 		}else{
 			$('.wrap-options').addClass('uk-hidden')
 		}
-	})
+	})*/
 
-	$('.wrap-options i').on('click', function(){
-		var option = '<input type="text" class="uk-input" name="options[]" placeholder="Digite a opção"/>';
-		$('.wrap-options-ch').append(option)
+	var htmlDrag = '<li class="uk-nestable-item">'+
+			'<div class="uk-icon-arrows item-draggable"></div>'+
+        	'</li>';
+
+	$('.uk-nestable').on('start.uk.nestable', function(ev, obj){
+		value = $('#columns').val()
+		arrValue = value.split(' ')
+
+		var html = '<div class="uk-grid">';
+		for(var i = 0; i < arrValue.length; i++){
+			html += '<div class="uk-width-'+arrValue[i]+'"></div>';
+		}
+		html += '</div>';
+
+		finalHtml += html;
+
+		obj.find('li').html(html)
+
+		$('ul.nestable-left').html(htmlDrag)
 	})
 
 	$('button').on('click', function(){
+		alert(finalHtml)
+	})
+
+
+
+
+	$('.uk-nestable').on('stop.uk.nestable', function(ev, obj, type){
+		console.log(type.find('div').attr('data-id'))
+
+	})
+	
+
+	/*$('button').on('click', function(){
+		$('.uk-form-controls').each(function(){
+			var valor = $(this).find('input.uk-input').val(),
+				columns = '';
+
+			if(valor != '' && !$(this).hasClass('used') ){
+				arrValor = valor.split(',');
+				if(arrValor.length > 0 && arrValor[0] !=''){
+					for (var i =  0; i < arrValor.length; i++) {
+						columns += '<div class="uk-width-'+arrValor[i].trim()+'"></div>';
+					}
+
+					var html =	'<div class="uk-container uk-container-center">'+
+								'<div class="uk-grid">'+
+								columns+
+								'</div>'+
+								'</div>';
+				}
+				
+				$('#wrap-result .uk-placeholder').each(function(){
+					if($(this).hasClass('empty-placeholder')){
+						$(this).html(html)
+						$(this).removeClass('empty-placeholder')
+						return false;
+					}
+				})	
+				$(this).addClass('used')
+			}
+
+		})
+	})*/
+
+/*	$('.wrap-options i').on('click', function(){
+		var option = '<input type="text" class="uk-input" name="options[]" placeholder="Digite a opção"/>';
+		$('.wrap-options-ch').append(option)
+	})*/
+
+/*	$('button').on('click', function(){
 		var $result = $('#result'), 
 		    tamanho = $('#tamanho').val(),
 		    tipo = $("#radio-tipo input[type='radio']:checked").val(),
@@ -58,12 +124,12 @@ $(document).ready(function(){
 		    $result.val(strFinal)
 
 
-	})
+	})*/
 
-	function createSlug(label){
+/*	function createSlug(label){
 		var id = label.toLowerCase(),
         	id = id.replace(/[^a-zA-Z0-9]+/g,'_');
 
         return id;	
-	}
+	}*/
 })	
